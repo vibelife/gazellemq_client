7817c80a988024ec1ee181fc4677d47c869fc553
gamedev8 <andre_newman2@hotmail.com>
2023-08-03 18:00:53 -0400

    More performance tuning #BG-128

--- publisher/main.cpp
+++ publisher/main.cpp
@@ -12,7 +12,7 @@ int main() {
 
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
-    // client.publish("test1", R"({"email":"andre_newman2@hotmail.com","password":"28077485"})");
+    // client.publish("test1", R"({"email":"diane@akanewmedia.com","password":"password123"})");
     // client.publish(888, R"({"email":"paul@akanewmedia.com","password":"metallica"})");
 
     auto t = std::chrono::high_resolution_clock::now().time_since_epoch();
@@ -20,7 +20,7 @@ int main() {
     printf("Current time: %zu\n", ms.count());
 
     for (int i{}; i < 1; ++i) {
-        client.publish("test1", R"({"email":"andre_newman2@hotmail.com","password":"28077485"})");
+        client.publish("test1", R"({"email":"diane@akanewmedia.com","password":"password123"})");
     }
 
     double elapsed = std::chrono::duration<double>{std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - t1)}.count();
